02a20cc940e1e0282709b89f301c9e25f535d940
Semyon Chaichenets <semyon@semyon.org>
2014-10-19 15:42:55 -0600

    plot 2 with source

---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Read in the data
+
+d <- read.csv("./household_p_c_small.txt",sep=";")
+d$Date <- as.Date(d$Date,format="%d/%m/%Y")
+d$Global_active_power <- as.double(d$Global_active_power)
+
+# subset to only the dates in question
+e <- d[d$Date >= as.Date("2007-02-01") & d$Date <= as.Date("2007-02-02"),]
+e$Time <- strptime(paste(as.character(e$Date),as.character(e$Time)),format="%Y-%m-%d %T")
+
+# plot the histogram, output device PNG
+
+png(filename="./plot2.png")
+plot(e$Time,e$Global_active_power,type='l',xlab="",ylab="Global active power (kilowatts)",main="Global Active Power")
+dev.off()
